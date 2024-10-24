6d1ed49339535e0db7ad0a4be28fb6a4769bf8c2
Tblizypy <tblizfx@gmail.com>
2024-10-25 00:55:47 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Update the package list
-apt-get update
+# Install dependencies
+apt-get update && apt-get install -y wget unzip
 
-# Install wget and unzip
-apt-get install -y wget unzip
-
-# Install Google Chrome
+# Download Google Chrome (use a pre-built version)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Install ChromeDriver
-CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+')
-wget https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip
+# Download ChromeDriver (hardcode a specific version to avoid mismatches)
+CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
+
+# Clean up
+rm google-chrome-stable_current_amd64.deb chromedriver_linux64.zip
