30048aaf5d2831831ba91aeba86a7f0b939c6f52
Tblizypy <tblizfx@gmail.com>
2024-10-31 01:50:04 +0100

    app.py

--- app.py
+++ app.py
@@ -25,12 +25,26 @@ TARGET_URL = 'https://www.sbobet.com/betting.aspx'
 def proxy(path):
     target_url = f'{TARGET_URL}/{path}'
     headers = {key: value for key, value in request.headers if key != 'Host'}
+
     if request.method == 'POST':
         response = requests.post(target_url, headers=headers, data=request.form)
     else:
         response = requests.get(target_url, headers=headers, params=request.args)
+
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
     headers = [(name, value) for (name, value) in response.raw.headers.items() if name.lower() not in excluded_headers]
+
+    # Modify response content for HTML pages
+    if 'text/html' in response.headers.get('Content-Type', ''):
+        content = response.content.decode('utf-8')
+
+        # Update links in the HTML content
+        content = content.replace('href="', f'href="{request.url_root}')
+        content = content.replace('src="', f'src="{request.url_root}')
+
+        response_content = content.encode('utf-8')
+        return Response(response_content, response.status_code, headers)
+
     return Response(response.content, response.status_code, headers)
 
 if __name__ == '__main__':
